9d4670e4b830579bfae6992b3bdad994ec6d058a
Arsalan khan <12khanarsalan8285@gmail.com>
2026-07-29 16:34:15 +0530

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
+const express = require("express");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("NOVIS Backend Running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
